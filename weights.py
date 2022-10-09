89cf96222701d55761f444e8d119fa12ffcdfa23
Liplum <Li_plum@outlook.com>
2022-10-09 17:29:54 +0800

    Customizable tags.

--- weights.py
+++ weights.py
@@ -1,10 +1,8 @@
 from tags import *
-
+"""
+You can customize tags here.
+"""
 all_tags = [
-    StaticTagType("ftype", weight=2000),
-    StaticTagType("activity", weight=600),
-    StaticTagType("freshman", weight=500),
-    StaticTagType(["expense", "tracker"], weight=500),
     LengthTagType(factor=-10),
     AnyTagType(weight=10000),
 ]
